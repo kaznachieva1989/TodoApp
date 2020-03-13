5ae7ef7b03bbdab838b111511279f085c67e47b3
kaznachieva1989 <kaznachieva1989@gmail.com>
2020-03-13 23:25:30 +0600

    add Storage

--- app/src/main/java/com/example/todoapp/ProfileActivity.java
+++ app/src/main/java/com/example/todoapp/ProfileActivity.java
@@ -1,35 +1,64 @@
+
 package com.example.todoapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity {
     ImageView imageView;
-    Uri imageData;
+    Uri imgUri;
     SharedPreferences preferences;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+
+    private static final String KEY_NAME = "name";
+    EditText myProfile;
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    DocumentReference noteRef = firebaseFirestore.document("users/My Profile");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        myProfile = findViewById(R.id.myProfile);
+        saveToFirestore();
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         imageView = findViewById(R.id.image_profile);
         preferences = getSharedPreferences("ava", MODE_PRIVATE);
@@ -46,13 +75,73 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
+    private void saveToFirestore() {
+        noteRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    String nameDoc = documentSnapshot.getString(KEY_NAME);
+
+                    myProfile.setText(nameDoc);
+                } else {
+                    Toast.makeText(ProfileActivity.this, "not exist", Toast.LENGTH_SHORT).show();
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 47 && resultCode == RESULT_OK && data != null) {
-            imageData = data.getData();
-            preferences.edit().putString("ava", String.valueOf(imageData)).apply();
-            Glide.with(this).load(imageData).into(imageView);
+            imgUri = data.getData();
+            preferences.edit().putString("ava", String.valueOf(imgUri)).apply();
+            Glide.with(this).load(imgUri).into(imageView);
+
+            StorageReference ref = storageReference.child("Photos").child(imgUri.getLastPathSegment());
+
+            ref.putFile(imgUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            Toast.makeText(ProfileActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+
+                        }
+                    });
         }
+
+    }
+
+
+    public void onClick1(View view) {
+        String name = myProfile.getText().toString();
+        Map<String, Object> note = new HashMap<>();
+        note.put(KEY_NAME, name);
+        noteRef.set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(ProfileActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ProfileActivity.this, "Failure", Toast.LENGTH_SHORT).show();
+                        Log.d("ololo", e.toString());
+                    }
+                });
     }
 }
